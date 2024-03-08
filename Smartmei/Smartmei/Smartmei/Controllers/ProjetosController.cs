bfae5de7dfdc1d3ee9dcfa4c67f084b1fb23bf86
Daiane Procópio <procopiodaianec@gmail.com>
2024-03-08 14:13:53 -0300

    Correção versão pacotes

---
+++ Smartmei/Smartmei/Smartmei/Controllers/ProjetosController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Smartmei.Models;
+
+namespace Smartmei.Controllers
+{
+    public class ProjetosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProjetosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Projetos
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Projetos;//.Include(p => p.Cliente).Include(p => p.Mei);
+            return View(await appDbContext.ToListAsync());
+        }
+    }
+}
