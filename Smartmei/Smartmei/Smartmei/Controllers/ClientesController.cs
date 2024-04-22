e4c685969fbe0808fc675e218f23a45dd47d2067
Juliana Calazans <100734910+JulianaCalazans@users.noreply.github.com>
2024-04-22 16:03:01 -0300

    Update- Página Cliente e Evento

--- Smartmei/Smartmei/Smartmei/Controllers/ClientesController.cs
+++ Smartmei/Smartmei/Smartmei/Controllers/ClientesController.cs
@@ -1,140 +1,149 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Smartmei.Models;
+using SmartMei.Models;
 
 namespace Smartmei.Controllers
 {
     public class ClientesController : Controller
     {
-
         private readonly AppDbContext _context;
+
         public ClientesController(AppDbContext context)
-
         {
-
             _context = context;
-
         }
 
+        // GET: Clientes
         public async Task<IActionResult> Index()
         {
-
-            var appDbContext = await _context.Clientes.ToListAsync();
-
-            return View(appDbContext);
+            return View(await _context.Clientes.ToListAsync());
         }
 
-        public IActionResult Create()
+        // GET: Clientes/Details/5
+        public async Task<IActionResult> Details(int? id)
         {
-
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Create(Cliente cliente)
-        {
-            if (ModelState.IsValid)
+            if (id == null)
             {
-                _context.Clientes.Add(cliente);
-                await _context.SaveChangesAsync();
-                return RedirectToAction("Index");
+                return NotFound();
+            }
+
+            var cliente = await _context.Clientes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cliente == null)
+            {
+                return NotFound();
             }
 
             return View(cliente);
         }
 
-        public async Task<IActionResult> Edit(int? id)
+        // GET: Clientes/Create
+        [HttpGet]
+        public IActionResult Create()
         {
-
-            if (id == null)
-                return NotFound();
-
-            var dados = await _context.Clientes.FindAsync(id);
-
-            if (dados == null)
-                return NotFound();
-
-
-
-            return View(dados);
+            return View(new Cliente()); 
         }
-
         [HttpPost]
-
-        public async Task<IActionResult> Edit(int id, Cliente cliente)
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,CNPJ,Telefone,Email,Cidade,Estado,ProductOwner,Observacao")] Cliente cliente)
         {
-            if (id != cliente.Id)
-                return NotFound();
+            if (cliente == null)
+            {
+                return BadRequest(); 
+            }
 
             if (ModelState.IsValid)
             {
-                _context.Clientes.Update(cliente);
+                _context.Add(cliente);
                 await _context.SaveChangesAsync();
-                return RedirectToAction("Index");
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cliente);
+        }
+
+        // GET: Clientes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
             }
 
-            return View();
-        }
-
-        public async Task<IActionResult> Details (int ? id)
-           {
-            if (id == null)
+            var cliente = await _context.Clientes.FindAsync(id);
+            if (cliente == null)
+            {
                 return NotFound();
-
-            var dados = await _context.Clientes.FindAsync(id);
-
-            if(dados == null)
-                return NotFound() ;
-
-
-
-            return View(dados);
+            }
+            return View(cliente);
         }
 
+        // POST: Clientes/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,CNPJ,Telefone,Email,Cidade,Estado,ProductOwner,Observacao")] Cliente cliente)
+        {
+            if (id != cliente.Id)
+            {
+                return NotFound();
+            }
 
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cliente);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClienteExists(cliente.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cliente);
+        }
+
+        // GET: Clientes/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
+            {
                 return NotFound();
+            }
 
-            var dados = await _context.Clientes.FindAsync(id);
-
-            if (dados == null)
+            var cliente = await _context.Clientes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cliente == null)
+            {
                 return NotFound();
+            }
 
-
-            return View(dados);
+            return View(cliente);
         }
 
-
-
+        // POST: Clientes/Delete/5
         [HttpPost, ActionName("Delete")]
-        public async Task<IActionResult> DeleteConfirmed(int? id)
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            if (id == null)
-                return NotFound();
-
-            var dados = await _context.Clientes.FindAsync(id);
-
-            if (dados == null)
-                return NotFound();
-
-            _context.Clientes.Remove(dados);
+            var cliente = await _context.Clientes.FindAsync(id);
+            _context.Clientes.Remove(cliente);
             await _context.SaveChangesAsync();
-
-
-            return RedirectToAction("Index");
+            return RedirectToAction(nameof(Index));
         }
 
-
+        private bool ClienteExists(int id)
+        {
+            return _context.Clientes.Any(e => e.Id == id);
+        }
     }
-                
-     }
-
-
-
-
-
-
-
-
+}
