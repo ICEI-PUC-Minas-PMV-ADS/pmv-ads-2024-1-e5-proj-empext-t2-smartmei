6a21fdcacdcc6f35dea2be6bf5d43ec8b9fd29d1
Daiane Procópio <procopiodaianec@gmail.com>
2024-05-28 19:13:16 -0300

    Correção Footer e pt-br.

--- Smartmei/Smartmei/Smartmei/Program.cs
+++ Smartmei/Smartmei/Smartmei/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using Smartmei.Models;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,6 +28,15 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
         options.LoginPath = "/Meis/Login";
     });
 
+// Configurar a cultura padrão
+var defaultCulture = new CultureInfo("pt-BR");
+var localizationOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(defaultCulture),
+    SupportedCultures = new List<CultureInfo> { defaultCulture },
+    SupportedUICultures = new List<CultureInfo> { defaultCulture }
+};
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -32,6 +44,16 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+// Use a configuração de localização
+app.UseRequestLocalization(localizationOptions);
+
+// Configuração do pipeline HTTP
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
 
